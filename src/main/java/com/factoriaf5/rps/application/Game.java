db33c35821629cb3a8b2bb51535087651e77bdcd
JohnnyRamirezSancho <info@johnnyramirez.es>
2023-01-27 08:49:28 +0100

    Player 1: rock

--- src/main/java/com/factoriaf5/rps/application/Game.java
+++ src/main/java/com/factoriaf5/rps/application/Game.java
@@ -1,5 +1,56 @@
 package com.factoriaf5.rps.application;
 
+import com.factoriaf5.rps.models.Figure;
+import com.factoriaf5.rps.models.Player;
+
 public class Game {
+
+    private Player player1;
+    private Player player2;
+    private Figure figure1;
+    private Figure figure2;
+    private String winner;
+       
+    public Game(Player player1, Player player2, String figurePlayer1, String figurePlayer2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.figure1 = this.player1.choose(figurePlayer1);
+        this.figure2 = this.player2.choose(figurePlayer2);
+        if(this.figure1.getType()=="roca")
+            if(this.figure2.getType()=="tijeras") {
+                winner =  "Player1 win!";
+            }
+            if(this.figure2.getType()=="roca") {
+                winner =  "Player1 and Player2, tied!";
+            }
+            if(this.figure2.getType()=="papel") {
+                winner =  "Player2 win!";
+            }
+        }
     
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
+
+    public Player getPlayer1() {
+        return player1;
+    }
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public Figure getFigure1() {
+        return figure1;
+    }
+
+    public Figure getFigure2() {
+        return figure2;
+    }
+
 }
--- src/test/java/com/factoriaf5/rps/application/GameTest.java
+++ src/test/java/com/factoriaf5/rps/application/GameTest.java
@@ -1,13 +1,44 @@
 package com.factoriaf5.rps.application;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 
 import org.junit.Test;
 
+import com.factoriaf5.rps.models.Figure;
+import com.factoriaf5.rps.models.Player;
+
 public class GameTest {
-    
+    Player player1 = new Player();
+    Player player2 = new Player();
+
     @Test
-    public void justASimpleTest() {
-        assertTrue(true);
+    public void game_have_two_players() {
+        Game newGame = new Game(player1, player2, "roca", "tijeras");
+        assertEquals(player1, newGame.getPlayer1());
+        assertEquals(player2, newGame.getPlayer2());
+    }
+
+    @Test
+    public void player1_choose_rock_and_player2_choose_scissors() {
+        Game newGame = new Game(player1, player2, "roca", "tijeras");
+        assertEquals("roca", newGame.getFigure1().getType());
+        assertEquals("tijeras", newGame.getFigure2().getType());
+    }
+
+    @Test
+    public void player1_choose_rock_and_player2_choose_scissors_win_player1() {
+        Game newGame = new Game(player1, player2, "roca", "tijeras");
+        assertEquals("Player1 win!", newGame.getWinner());
+    }
+
+    public void player1_choose_rock_and_player2_choose_paper_win_player2() {
+        Game newGame = new Game(player1, player2, "roca", "papel");
+        assertEquals("Player2 win!", newGame.getWinner());
+    }
+
+    public void player1_choose_rock_and_player2_choose_rock_then_tied() {
+        Game newGame = new Game(player1, player2, "roca", "papel");
+        assertEquals("Player1 and Player2, tied!", newGame.getWinner());
     }
 }
